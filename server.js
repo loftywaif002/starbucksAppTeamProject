309e8176e622b03b103cf1fe708b53f32e450dd3
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2017-04-09 02:14:02 -0700

    server modified

--- server.js
+++ server.js
@@ -1,12 +1,58 @@
-const express = require('express');
-const app = express();
-var path    = require("path");
+var express = require('express');
+var browserify = require('browserify');
+var React = require('react');
+var jsx = require('node-html');
+var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+//jsx.install();
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/'+'/public/index.html'));
-})
+var Books = require('./public/index.html');
+
+app.use('/bundle.js', function(req, res) {
+  res.setHeader('content-type', 'application/javascript');
+  browserify('./app.js', {
+    debug: true
+  })
+  .transform('reactify')
+  .bundle()
+  .pipe(res);
+});
+
+app.use('/', function(req, res) {
+  var books = [{
+    title: 'Professional Node.js',
+    read: false
+  }, {
+    title: 'Node.js Patterns',
+    read: false
+  }];
+
+  res.setHeader('Content-Type', 'text/html');
+  res.end(React.renderToStaticMarkup(
+    React.DOM.body(
+      null,
+      React.DOM.div({
+        id: 'container',
+        dangerouslySetInnerHTML: {
+          __html: React.renderToString(React.createElement(Books, {
+            books: books
+          }))
+        }
+      }),
+      React.DOM.script({
+        'id': 'initial-data',
+        'type': 'text/plain',
+        'data-json': JSON.stringify(books)
+      }),
+      React.DOM.script({
+        src: '/bundle.js'
+      })
+    )
+  ));
+});
+
+var server = app.listen(3333, function() {
+  var addr = server.address();
+  console.log('Listening @ http://%s:%d', addr.address, addr.port);
+});
