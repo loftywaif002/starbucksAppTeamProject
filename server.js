8d320446d9d6638b26233fab9873bab882886bce
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2017-04-11 21:26:55 -0700

    New React Component Added

--- server.js
+++ server.js
@@ -1,66 +1,56 @@
-require('import-export')
-require('babel-core/register')({ presets: ['es2015', 'react'] })
+//server.js
+'use strict'
+//first we import our dependencies…
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
-const express = require('express')
-const react = require('react')
-const reactDomServer = require('react-dom/server')
-const reactRouter = require('react-router')
+var Order = require('./model/orders');
 
-const renderToString = reactDomServer.renderToString
-const match = reactRouter.match
-const RouterContext = reactRouter.RouterContext
+//and create our instances
+var app = express();
+var router = express.Router();
+//set our port to either a predetermined port number if you have set 
+//it up, or 3001
+var port = process.env.API_PORT || 3001;
 
-const staticFiles = [
-  '/static/*',
-  '/logo.svg',
-  '/asset-manifest.json',
-  '/favicon.ico'
-]
+var mongoURI = "mongodb://127.0.0.1:27017/starbucksDB";
+var MongoDB = mongoose.connect(mongoURI).connection;
+MongoDB.on('error', function(err) { console.log(err.message); });
+MongoDB.once('open', function() {
+  console.log("mongodb connection is open");
+});
+
+//now we should configure the API to use bodyParser and look for 
+//JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+//now we can set the route path & initialize the API
+router.get('/', function(req, res) {
+ res.json({ message: 'API Initialized!'});
+});
 
 
-const app = express()
-app.server = http.createServer(app)
-app.use(express.static('../build'))
 
-staticFiles.forEach(file => {
-  app.get(file, (req, res) => {
-    const filePath = path.join( __dirname, '../build', req.url )
-    res.sendFile( filePath )
-  })
-})
 
-app.get('*', (req, res) => {
 
-  const error = () => res.status(404).send('404')
-  const htmlFilePath = path.join( __dirname, '../build', 'index.html' )
 
-  fs.readFile( htmlFilePath, 'utf8', (err, htmlData) => {
-    if(err) {
-      error()
-    }
-    else {
-      match({ routes, location: req.url }, (err, redirect, ssrData) => {
-        if(err) {
-          error()
-        }
-        else if(redirect) {
-          res.redirect(302, redirect.pathname + redirect.search)
-        }
-        else if(ssrData) {
-          const ReactApp = renderToString( react.createElement(RouterContext, ssrData) )
-          const RenderedApp = htmlData.replace('{{SSR}}', ReactApp)
-          res.status(200).send(RenderedApp)
-        }
-        else {
-          error()
-        }
-      })
-    }
-  })
-})
-
-app.server.listen( process.env.PORT || 8080 )
-console.log(`Listening on http://localhost:${app.server.address().port}`)
+//Use our router configuration when we call /api
+app.use('/api', router);
+//starts the server and listens for requests
+app.listen(port, function() {
+ console.log(`api running on port ${port}`);
+});
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,168 @@
 import React, {Component} from 'react';
 import 'react-materialize';
 import Carousel from './Carousel';
-import { Row, Col, Grid, Thumbnail, Button } from 'react-bootstrap';
-import ThumbNailSquare from './ThumbNailSquare';
+import { 
+  Row, 
+  Col, 
+  Grid, 
+  Thumbnail, 
+  Button,
+  ButtonGroup,
+  DropdownButton,
+  MenuItem,
+  FormControl,
+  FormGroup,
+  ControlLabel,
+  Form
+} from 'react-bootstrap';
+
+import {
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalClose,
+  ModalBody,
+  ModalFooter 
+} from 'react-modal-bootstrap';
+
 
 
 class Main extends Component {
     constructor (props) {
         super(props);
-       
+            this.state = {
+               isOpen: false,
+               qty: '1',
+               name: ''
+           }
+          this.openModal = this.openModal.bind(this);
+          this.hideModal = this.hideModal.bind(this);
+          this.setQuantity = this.setQuantity.bind(this); 
       }
 
-    render () {
-        return (
-        	 <div> 
-           <Carousel />
-           <ThumbNailSquare sanjoseDescription="10% Disctount for SJSU students"/>
+ 
+openModal = () => {
+  this.setState({
+    isOpen: true
+  });
+}
+
+hideModal = () => {
+  this.setState({
+    isOpen: false,
+    
+  });
+}
+
+setQuantity(event){
+event.preventDefault();
+this.setState({qty: event.target.value});
+
+}
+
+
+
+
+render () {
+    return (
+        <div> 
+          <Carousel />
+<Modal isOpen={this.state.isOpen} onRequestHide={this.hideModal}>
+  <ModalHeader>
+    <ModalClose onClick={this.hideModal}/>
+    <ModalTitle>Place Your Order</ModalTitle>
+  </ModalHeader>
+  <ModalBody>
+
+  <Form horizontal>
+      <h3 bsStyle="success">Store: San Jose</h3>
+  <FormGroup controlId="formControlsSelect">
+      <h5 >Number of Items</h5>
+      <FormControl componentClass="select" placeholder="select" onChange={this.setQuantity} value={this.state.qty}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </FormControl>
+      <h5 >HotDrinks</h5>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="RegularCoffee">Regular Coffee</option>
+        <option value="Decaf">Decaf</option>
+        <option value="Americano">Americano</option>
+        <option value="Cappccuino">Cappccuino</option>
+        <option value="WhiteMocha">White Mocha</option>
+        <option value="CafeLatte">CafeLatte</option>
+      </FormControl>
+      <h5 >Iced Drinks</h5>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="IcedCaffeAmericano">Iced Caffe Americano</option>
+        <option value="IcedCaffeLatte">Iced Caffe Latte</option>
+        <option value="IcedCaffeMocha">Iced Caffe Mocha</option>
+        <option value="Iced Caramel Machiato">Iced Caramel Machiato</option>
+        <option value="IcedVanillaLatte">Iced Vanilla Latte</option>
+      </FormControl>
+        <h5 >Milk Type (Does not apply for Iced Drknks)</h5>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="Full Cream">Full Cream</option>
+        <option value="Half-n-Half">Half-n-Half</option>
+      </FormControl>
+      <h5 >Size</h5>
+      <FormControl componentClass="select" placeholder="select">
+        <option value="tall">tall</option>
+        <option value="Grande">Grande</option>
+        <option value="Venti">Venti</option>
+      </FormControl>
+  </FormGroup>
+</Form>
+
+  </ModalBody>
+  <ModalFooter>
+    <button className='btn btn-default' onClick={this.hideModal}>
+      Cancel
+    </button>
+    <button className='btn btn-primary'>
+      Place Order
+    </button>
+  </ModalFooter>
+</Modal>
+           
+<Grid>
+    <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image3.JPG" alt="242x200">
+        <h3>Order At San jose</h3>
+        <p>10% Disctount for SJSU students</p>
+        <p>
+          <Button bsStyle="primary" onClick={this.openModal}>Order</Button>&nbsp;
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image1.jpg" alt="242x200">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+        <h3>Order At San Francisco</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Order</Button>&nbsp;
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image2.JPG" alt="242x200">                                                                                                                                                                                                                                                                                                                                                
+        <h3>Order At Palo Alto</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Order</Button>&nbsp;
+        </p>
+      </Thumbnail>
+    </Col>
+    </Row>
+  </Grid>
            </div>
         );
     }
